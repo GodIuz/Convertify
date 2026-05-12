140fd37ca2c200fa6393a1c21674892d8b201b2e
GodIuz <notyetv@gmail.com>
2026-05-12 06:38:25 +0300

    Fixing issue with FFPEG lib

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
-import ffmpeg_kit_flutter_full_gpl
+import ffmpeg_kit_flutter_new
 import file_picker
 import file_saver
 import flutter_image_compress_macos
